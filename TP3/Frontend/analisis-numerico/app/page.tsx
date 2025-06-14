9b4ffdc197028fd669e3166a7ce58e794735d766
Diego Danelone <ddanelone@frsf.utn.edu.ar>
2025-06-14 16:09:52 -0300

    Frontend depurado. Menú principal y ruteo listo. Joya, nunca taxi.

---
+++ TP3/Frontend/analisis-numerico/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>Hola, Análisis Numérico 2025</div>;
+}
---
+++ TP3/Frontend/analisis-numerico/components/logo.tsx
@@ -0,0 +1,31 @@
+const Logo = () => {
+  return (
+    <div className="flex flex-col items-center w-24 text-xs font-medium">
+      <svg
+        className="w-10 h-10 mb-1"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M2 6C2 2.68629 4.68629 0 8 0C11.3137 0 14 2.68629 14 6V16H12L10 14L8 16L6 14L4 16H2V6ZM7 6C7 6.55228 6.55228 7 6 7C5.44772 7 5 6.55228 5 6C5 5.44772 5.44772 5 6 5C6.55228 5 7 5.44772 7 6ZM10 7C10.5523 7 11 6.55228 11 6C11 5.44772 10.5523 5 10 5C9.44772 5 9 5.44772 9 6C9 6.55228 9.44772 7 10 7Z"
+            fill="white"
+          />
+        </g>
+      </svg>
+      <p className="text-center">Análisis Numérico 2025</p>
+      <br />
+    </div>
+  );
+};
+
+export default Logo;
